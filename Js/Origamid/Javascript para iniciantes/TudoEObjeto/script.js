966981a526a0f13b56b7190d189af0f58ce89b5a
Luan-Lopes-da-Silva <lopesluan18@outlook.com>
2023-03-01 09:49:41 -0300

    Exercicio mostrando que a importancia de object em js é muito grande

---
+++ Js/Origamid/Javascript para iniciantes/TudoEObjeto/script.js
@@ -0,0 +1,14 @@
+// nomeie 3 propriedades ou métodos de strings
+//Lenght
+//Match
+//CharAt
+// nomeie 5 propriedades ou métodos de elementos do DOM
+//Classlist
+//Classname
+//innerHTML
+//addEventListener
+//nextSibling
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+//Select
+
