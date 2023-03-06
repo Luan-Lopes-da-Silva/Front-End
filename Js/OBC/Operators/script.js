46c36c34bb2051e3fd75f1c6b0b84776652a4003
Luan-Lopes-da-Silva <lopesluan18@outlook.com>
2023-03-06 09:06:07 -0300

    Operators, Variables and types of datas

---
+++ Js/OBC/Operators/script.js
@@ -0,0 +1,38 @@
+//Sum
+console.log(5+5)
+
+//Subtraction
+console.log(5-5)
+
+//Times
+console.log(5*5)
+
+//Division
+console.log(25/5)
+
+//Rest of division
+console.log(10%3)
+
+//Agroupment
+console.log(3+3*5)//18
+console.log((3+3)*5)//30
+//With agroupment the JS from the priority for what it is in brackets
+
+//Attribution
+let a = 5
+a = a + 3
+console.log(a)
+//Simplifying
+a+=3
+console.log(a)
+//This type of attribution serve for all other operators
+
+//Increment
+let b = 2
+console.log(b)
+b++
+console.log(b)
+
+//Decrement
+b--
+console.log(b)
---
+++ Js/OBC/Variables/script.js
@@ -0,0 +1,36 @@
+//Var
+var name = 'Luan'
+console.log(name)
+var name = 'José'
+console.log(name)
+//Allows for reassignment of value
+//Don´t show error of identfier declared
+
+//Let
+let age = 23
+console.log(age)
+//Allows for reassignment of value
+//let age = 24
+//Show error of identfier declared
+age = 25
+console.log(age)
+//Correct shape of reassignment of value in let variable
+
+
+//Const
+const genre = 'Masculine'
+console.log(genre)
+// Don´t Allows for reassignment of value
+
+//Name of variables
+//The ideal is what variables have a names what do sense for code in general avoid variables minimalists
+//Try describle the maximun for what a variable it suits.
+
+
+//Cases
+//Camel case
+const myAgeCurrent = 23
+console.log(myAgeCurrent)
+//Snake case
+const my_age_current = 23
+console.log(my_age_current)
