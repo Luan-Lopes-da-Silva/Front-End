0601352c38ae4aedc6ab8b7afe8bb3e3478c8524
Luan-Lopes-da-Silva <lopesluan18@outlook.com>
2023-04-08 20:49:39 -0300

    Globals objects

---
+++ Js/OBC/Globals objects/index.js
@@ -0,0 +1,29 @@
+//Object
+//Used for create object item
+const object = Object()
+object.name = 'Luan'
+object.age = 22
+console.log(object)
+//Array
+//Used for create array
+const array = Array('Luan','José','Kauan','Maria')
+console.log(array)
+//String
+//Used for create a string or convert one number or other thing in a string
+const string = String('Luan is my name too')
+console.log(string)
+const convertString = String(object.age)
+console.log(convertString)
+//Number
+//Used for create one number or convert a string in one number
+const number = Number(42)
+console.log(number)
+const convertNumber = Number('52')
+console.log(convertNumber)
+//Date
+const dateTodayAndNow = Date()
+console.log(dateTodayAndNow)
+//Math
+//Used for to facilitate math functions
+const pi = Math.PI
+console.log(pi)
