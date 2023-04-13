f80ed8ddf08176500e3fe06c985b9f416b73702b
Luan-Lopes-da-Silva <lopesluan18@outlook.com>
2023-04-12 23:16:44 -0300

    End of module of programming logic

--- Js/OBC/Jobs/index.js
+++ Js/OBC/Jobs/index.js
@@ -1,73 +1,122 @@
-let interativeMenu = ''
-let detailsOfVacancy = []
-let indexFunction = ''
-let canditates = []
-let confirmation = ''
-let canditate = ''
-do{
-interativeMenu = prompt('Jobs\n a)Available vacancies\n b)Create a new vacancy\n c)View a vacancy\n d)Apply for a vacancy\n e)Exclude a vacancy\n f)Exit')
-switch(interativeMenu){
-  case 'a':
-  alert('You choice the option a')
-  break
-  case 'b':
-  const detailsObject = {}
-  detailsObject.name = prompt('What name for the vacancy')
-  detailsObject.description = prompt('Give a description for this vacancy')
-  detailsObject.limit = prompt('Which is the limit date for if application of this vacancy') 
-  detailsObject.index =  parseFloat(prompt('Which is the index of this element?'))
-  detailsOfVacancy.push(detailsObject)
-  console.log(detailsOfVacancy)
-  break
-  case 'c':
-    indexFunction = parseFloat(prompt('Which is the index of vacancy?'))
-    function checkIndex(){
-      for(index=0; index<=detailsOfVacancy.length;index++){
-        if(indexFunction == index){
-        alert(
-          '\nThe index of vacancy is ' + detailsOfVacancy[index].index+
-          '\nThe name of this vacancy is ' + detailsOfVacancy[index].name+
-          '\nyour description is ' + detailsOfVacancy[index].description+
-          '\nand your limit date for if candidate for this vacancy is ' + detailsOfVacancy[index].limit
-          )  
-        }
-        }
-    }
-    checkIndex()
-  break  
-  case 'd':
-  canditate = prompt('What is your name?')
-  indexFunction = parseFloat(prompt('Which is the index of vacancy?'))
-  function confirmApplicate(){
-    for(index=0; index<=detailsOfVacancy.length;index++){
-      if(indexFunction == index){
-     confirmation = confirm(
-        '\nIs for this vacancy wich you wish to applicate? '+ canditate+
-        '\nThe name of this vacancy is ' + detailsOfVacancy[index].name+
-        '\nyour description is ' + detailsOfVacancy[index].description+
-        '\nand your limit date for if candidate for this vacancy is ' + detailsOfVacancy[index].limit
-        )  
-      }
-      }
-      if(confirmation){
-        alert('Finishing your candidature one moment....')
-        canditates.push(canditate)
-      }
-    }
+const vacancies = []
 
-     
-  confirmApplicate()
- 
-  break  
-  case 'e':
-  alert('You choice the option e')
-  break  
-  case 'f':
-  alert('You choiced exit of application')
-  break  
-  default:
-  alert('Incorrect option')
-  break
+function listVacancies(){
+  const textVacancies = vacancies.reduce(function(finalText,vacancy,index){
+    finalText+=index+ '. '
+    finalText+=vacancy.name
+    finalText+= ' (' + vacancies[index].candidates.length + ' candidates)\n'
+    return finalText
+  }, '')
+  alert(textVacancies)
 }
-}while(interativeMenu!=='f')
-alert('Exiting.....')
+function newVacancy(){
+  const name = prompt('What is the name of vacancy? ')
+  const descript = prompt('What is the description of this vacancy? ')
+  const limitDate = prompt('What is the limit date for if apply for this vacancy? ')
+  const confirmation = confirm(
+    'This is the informations of vacancy wich you wish create he is sure?\n'+
+    '\nThe name of vacancy is ' + name + 
+    '\nThe limit date for apply is ' + limitDate + 
+    '\nand your description is bellow ' + descript    
+    )
+    if(confirmation){
+      const newVacancy = {
+      name,
+      descript,
+      limitDate,
+      candidates:[]
+      }
+    vacancies.push(newVacancy)
+    }
+}
+
+  function viewVacancy(){
+  const index = prompt('What is the index of vacancy? ')
+  const vacancy = vacancies[index]
+  const candidateInText = vacancy.candidates.reduce(function(finalText,candidate){
+    return finalText+ "\n - " + candidate
+  },"")
+  alert(
+    'Vacancy ' + (index+1) + 
+    '\nName of vacancy: ' + vacancy.name + 
+    '\nDescription of vacancy: ' + vacancy.descript+
+    '\nLimit date: ' + vacancy.limitDate + 
+    '\nCandidates: ' + vacancy.candidates.length+
+    '\nRegistered candidates: ' + candidateInText
+    )
+  }
+
+  function registerNewCandidate(){
+  const candidate = prompt('What your name?')
+  const index = prompt('What is the index of vacancy?')
+  const vacancy = vacancies[index]
+  const confirmation = 
+  confirm(
+    'This is vacancy wich your wish apply ' + candidate + '?' +
+    '\nYour name is ' + vacancy.name + 
+    '\nyour description is ' + vacancy.descript + 
+    '\nand your limit date for apply is ' + vacancy.limitDate
+  )
+  if(confirmation){
+    vacancy.candidates.push(candidate)
+    alert('Subscribed successfully')
+  }
+  }
+
+  function deleteVacancy(){
+  const index = prompt('What is the index of vacancy?')
+  const vacancy = vacancies[index]
+  const confirmation = 
+  confirm(
+    'This is vacancy wich your wish delete?'+
+    '\nYour name is ' + vacancy.name + 
+    '\nyour description is ' + vacancy.descript + 
+    '\nand your limit date for apply is ' + vacancy.limitDate
+  )
+  if(confirmation){
+    vacancies.splice(index,1)
+    alert('Vacancy deleted with sucessfully!!')
+  }
+  }
+
+  function displayMenu(){
+  const option = 
+  prompt(
+  '\na)Available vacancies'+
+  '\nb)Create a new vacancy'+
+  '\nc)View vacancy'+
+  '\nd)Apply candidate'+
+  '\ne)Delete vacancy'+
+  '\nf)Exit'  
+  )
+  return option
+  }
+  function execute(){
+    let option = ''
+    do{
+    option = displayMenu()
+    switch(option){
+    case 'a':
+    listVacancies()
+    break
+    case 'b':
+    newVacancy()
+    break
+    case 'c':
+    viewVacancy()
+    break
+    case 'd':
+    registerNewCandidate()
+    break
+    case 'e':
+    deleteVacancy()
+    break
+    case 'f':
+    alert('Exiting of application....')
+    break  
+    }
+    }while(option!=='f')
+    alert('Bye!!!')
+  }
+
+  execute();
