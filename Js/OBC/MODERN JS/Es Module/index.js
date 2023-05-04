9e369acd678b594c04dc22bd8d7be6d7c6290740
Luan-Lopes-da-Silva <lopesluan18@outlook.com>
2023-05-04 09:37:09 -0300

    ES modules

---
+++ Js/OBC/MODERN JS/Es Module/index.js
@@ -0,0 +1,5 @@
+import {br,input,label } from "./functions.js"
+
+console.log(input({id:'fullName',innerText:'Full name',name:'fullName'}))
+console.log(label({for:'fullName',textContent:'Full name'}))
+console.log(br())
