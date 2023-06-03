156603c3db14241db76d3d63eb8f11cf7eea7b73
Luan-Lopes-da-Silva <lopesluan18@outlook.com>
2023-06-03 01:06:46 -0300

    End of exercise

--- Js/OBC/AVANCED TOPICS/Consuming a API/index.js
+++ Js/OBC/AVANCED TOPICS/Consuming a API/index.js
@@ -1,70 +1,134 @@
-function createBill(bill){
-const article = document.createElement('article')
-article.classList.add('finance')
-article.id = bill.id
-
-const name = document.createElement('h2')
-name.classList.add('name')
-name.textContent = bill.name
-
-const value = document.createElement('span')
-value.classList.add('value')
-value.textContent = bill.value
-
-article.append(name,value)
-document.querySelector('#finances').appendChild(article)
+function createTransactionContainer(id) {
+  const container = document.createElement("div");
+  container.classList.add("transaction");
+  container.id = `transaction-${id}`;
+  return container;
 }
 
-async function fetchFinances(){
-const finances = await fetch('http://localhost:3000/finances').then((res=>res.json()))
-finances.forEach(createBill)
+function createTransactionTitle(name) {
+  const title = document.createElement("span");
+  title.classList.add("transaction-title");
+  title.textContent = name
+  return title;
 }
-
-document.addEventListener('DOMContentLoaded', ()=>{
-  fetchFinances()
-})
-
-const form = document.querySelector('#form1')
-form.addEventListener('submit', async (ev)=>{
-ev.preventDefault();
-
-const bill = {
-  name: document.querySelector('#name').value,
-  value: document.querySelector('#value').value
-}
-
-const response = await fetch('http://localhost:3000/finances',{
-  method:'POST',
-  headers:{
-  'Content-Type':'application/json'  
-  },
-  body:JSON.stringify(bill)
-})
-const savedArticle = await response.json()
-form.reset()
-createBill(savedArticle)
-})
-
-
-
-const form2 = document.querySelector('#form2').addEventListener('submit', async (ev)=>{
-  ev.preventDefault();
-  const url = 'http://localhost:3000/finances'
-  const id = document.querySelector('#form2 #id')
-  
-fetch(url, {
-  method: 'PUT',
-  headers: {
-    'Content-Type': 'application/json'
-  }
-})
-.then(response => {
-  if (response.json().filter(bill=>bill.id == id.value)) {
-    console.log('Requisição PUT enviada com sucesso!');
+function createTransactionAmount(amount) {
+  const span = document.createElement("span");
+  span.classList.add("transaction-amount");
+  const formater = Intl.NumberFormat("pt-BR", {
+    compactDisplay: "long",
+    currency: "BRL",
+    style: "currency",
+  });
+  const formatedAmount = formater.format(amount);
+  if (amount > 0) {
+    span.textContent = `${formatedAmount} C`;
+    span.classList.add("credit");
   } else {
-    console.error('Erro ao enviar a requisição PUT:', response.status);
+    span.textContent = `${formatedAmount} D`;
+    span.classList.add("debit");
   }
-})
-})
+  return span;
+}
 
+function renderTransaction(transaction) {
+  const container = createTransactionContainer(transaction.id);
+  const title = createTransactionTitle(transaction.name);
+  const amount = createTransactionAmount(transaction.amount);
+  const editBtn = createEditTransactionBtn(transaction)
+  const deleteBtn = createDeleteTransactionBtn(transaction.id)
+  container.append(title, amount,editBtn,deleteBtn);
+  document.querySelector("#transactions").append(container);
+}
+
+async function fetchTransactions() {
+  return await fetch("http://localhost:3000/transactions").then((res) => res.json());
+}
+
+let transactions = [];
+
+function updateBalance() {
+  const balanceSpan = document.querySelector("#balance");
+  const balance = transactions.reduce(
+    (sum, transaction) => sum + transaction.amount,
+    0
+  );
+  const formater = Intl.NumberFormat("pt-BR", {
+    compactDisplay: "long",
+    currency: "BRL",
+    style: "currency",
+  });
+  balanceSpan.textContent = formater.format(balance);
+}
+
+async function setup() {
+  const results = await fetchTransactions();
+  transactions.push(...results);
+  transactions.forEach(renderTransaction);
+  updateBalance();
+}
+
+async function saveTransaction(ev) {
+  ev.preventDefault();
+
+  const id = document.querySelector("#id").value;
+  const name = document.querySelector("#name").value;
+  const amount = parseFloat(document.querySelector('#amount').value)
+
+  if (id) {
+    const response = await fetch(`http://localhost:3000/transactions/${id}`, {
+      method: "PUT",
+      body: JSON.stringify({ name, amount }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const transaction = await response.json();
+    const indexToRemove = transactions.findIndex((t) => t.id === id);
+    transactions.splice(indexToRemove, 1, transaction);
+    document.querySelector(`#transaction-${id}`).remove();
+    renderTransaction(transaction);
+  } else {
+    const response = await fetch("http://localhost:3000/transactions", {
+      method: "POST",
+      body: JSON.stringify({ name, amount }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const transaction = await response.json();
+    transactions.push(transaction);
+    renderTransaction(transaction);
+  }
+  ev.target.reset();
+  updateBalance();
+}
+
+function createEditTransactionBtn(transaction) {
+  const editBtn = document.createElement("button");
+  editBtn.classList.add("edit-btn");
+  editBtn.textContent = "Edit";
+  editBtn.addEventListener("click", () => {
+    document.querySelector("#id").value = transaction.id;
+    document.querySelector("#name").value = transaction.name;
+    document.querySelector("#amount").value = transaction.amount;
+  });
+  return editBtn;
+}
+
+function createDeleteTransactionBtn(id){
+  const deleteBtn = document.createElement("button")
+  deleteBtn.classList.add("delete-btn")
+  deleteBtn.textContent = "Delete"
+  deleteBtn.addEventListener("click",async ()=>{
+  await fetch(`http://localhost:3000/transactions/${id}`,{method:'DELETE'})
+  deleteBtn.parentElement.remove()
+  const indexToRemove = transactions.findIndex((t) => t.id === id);
+    transactions.splice(indexToRemove, 1);
+    updateBalance()
+  })
+  return deleteBtn
+}
+
+document.addEventListener("DOMContentLoaded", setup);
+document.querySelector("form").addEventListener("submit", saveTransaction);
 
